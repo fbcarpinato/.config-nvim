57f4c444c727fa86dace95dfc66146e933427bf0
Fabio Carpinato <fabiocarpinato@outlook.it>
2024-02-03 18:27:32 +0100

    initial setup of v2

---
+++ lua/fabiocarpinato/lazy/neotree.lua
@@ -0,0 +1,35 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    hide_by_name = {
+                        "node_modules",
+                        "git",
+                    },
+                    visible = true,
+                },
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = false
+                },
+            },
+            buffers = {
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = true
+                }
+            }
+        })
+
+        vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+    end
+}
