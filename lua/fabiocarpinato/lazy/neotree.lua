e4f3ca26fc8e6d900105984de201eadcb698604d
Fabio Carpinato <fabiocarpinato@outlook.it>
2024-02-28 13:38:19 +0100

    chore: restores neotree

---
+++ lua/fabiocarpinato/lazy/neotree.lua
@@ -0,0 +1,29 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					hide_by_name = {
+						"node_modules",
+						"git",
+					},
+					visible = true,
+				},
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>nt", "<cmd>Neotree toggle<CR>")
+	end,
+}
