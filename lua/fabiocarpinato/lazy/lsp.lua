6a4b78e78bac3eb10f298f28e2353fffa99c00bf
Fabio Carpinato <fabiocarpinato@outlook.it>
2025-06-21 10:35:42 +0200

    fix: temporary version pin to avoid mason breaking changes

--- lua/fabiocarpinato/lazy/lsp.lua
+++ lua/fabiocarpinato/lazy/lsp.lua
@@ -10,8 +10,8 @@ end
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    { "williamboman/mason.nvim",           version = "^1.0.0" },
+    { "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
