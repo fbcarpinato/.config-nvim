4c4ee7da486a00293377f5b4890366d05df8b8b7
Fabio Carpinato <fabiocarpinato@outlook.it>
2025-06-21 11:27:44 +0200

    feat: improves lsp configuration

--- lua/fabiocarpinato/lazy/lsp.lua
+++ lua/fabiocarpinato/lazy/lsp.lua
@@ -1,179 +1,65 @@
-local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = "",
-  }
-  vim.lsp.buf.execute_command(params)
-end
-
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "williamboman/mason.nvim",           version = "^1.0.0" },
-    { "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
-    "nvimtools/none-ls.nvim",
-  },
+	{
+		"saghen/blink.cmp",
+		build = "cargo build --release",
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			fuzzy = { implementation = "prefer_rust" },
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason.nvim", version = "^1.0.0" },
+			{ "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
+			"saghen/blink.cmp",
+		},
 
-  config = function()
-    local cmp = require("cmp")
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+		opts = {
+			servers = {
+				lua_ls = {},
+				rust_analyzer = {},
+				ts_ls = {},
+			},
+		},
 
-    vim.filetype.add({ extension = { templ = "templ" } })
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
 
-    require("fidget").setup({})
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "ts_ls",
-        "templ",
-        "gopls",
-        "tailwindcss",
-        "templ",
-        "html",
-        "clangd",
-      },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = vim.tbl_keys(opts.servers),
+			})
 
-        ["ts_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.ts_ls.setup({
-            capabilities = capabilities,
-            commands = {
-              OrganizeImports = {
-                organize_imports,
-                description = "Organize Imports",
-              },
-            },
-            init_options = {
-              hostInfo = "neovim",
-              preferences = {
-                importModuleSpecifierPreference = "relative",
-              },
-            },
-          })
+			for server, config in pairs(opts.servers) do
+				local capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
 
-          vim.keymap.set("n", "<leader>o", "<cmd>:OrganizeImports<CR>")
-        end,
-
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim", "it", "describe", "before_each", "after_each" },
-                },
-              },
-            },
-          })
-        end,
-      },
-    })
-    local lspconfig = require("lspconfig")
-    if not lspconfig then
-      lspconfig.c3_lsp = {
-        default_config = {
-          cmd = "c3lsp",
-          filetypes = { "c3", "c3i" },
-          settings = {},
-          name = "c3_lsp",
-        },
-      }
-    end
-    lspconfig.c3_lsp.setup({})
-
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-      sources = cmp.config.sources({
-        {
-          name = "nvim_lsp",
-          entry_filter = function(entry, ctx)
-            return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
-          end,
-        },
-        { name = "luasnip" }, -- For luasnip users.
-      }, {
-        { name = "buffer" },
-      }),
-    })
-
-    vim.diagnostic.config({
-      -- update_in_insert = true,
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    })
-
-    local null_ls = require("null-ls")
-
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.formatting.rustywind,
-        null_ls.builtins.formatting.phpcsfixer,
-        null_ls.builtins.formatting.clang_format,
-      },
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+				lspconfig[server].setup({
+					capabilities = capabilities,
+				})
+			end
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = { "stylua", "prettierd" },
+		},
+	},
 }
